f5257f6a6fe81b8d744e9a0ec650a5843edf59a4
Tearfall <108473064+Tearfall@users.noreply.github.com>
2023-10-16 07:35:45 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MauiApp5;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	public void OnVerticalStart(object sender, EventArgs e)
+	{
+		Target.VerticalOptions = LayoutOptions.Start;
+	}
+    public void OnVerticalCenter(object sender, EventArgs e)
+    {
+        Target.VerticalOptions = LayoutOptions.Center;
+    }
+    public void OnVerticalEnd(object sender, EventArgs e)
+	{
+        Target.VerticalOptions = LayoutOptions.End;
+    }
+
+	public void OnHorizontalStart(object sender, EventArgs e)
+	{
+		Target.HorizontalOptions = LayoutOptions.Start;
+	}
+    public void OnHorizontalCenter(object sender, EventArgs e)
+    {
+        Target.HorizontalOptions = LayoutOptions.Center;
+    }
+    public void OnHorizontalEnd(object sender, EventArgs e)
+    {
+        Target.HorizontalOptions = LayoutOptions.End;
+    }
+
+}
+
